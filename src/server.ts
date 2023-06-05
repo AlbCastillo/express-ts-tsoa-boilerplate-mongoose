6c6cc8da26f7bb52e44ba6a4a0ec7a741284d742
AlbCastillo <alberto.castillo@beoneoff.com>
2023-06-06 01:26:07 +0200

    chore: connect mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import app from './app';
 import { CONFIG } from './config';
 import logger from './logging/winstonLogger';
+import { connectMongoDB } from './mongoose';
 
 const port = CONFIG.API.PORT;
 
 /**
  * CONNECT DATABASE
  */
-// connectMongoDB();
+connectMongoDB();
 
 /**
  * SERVER RUNNING ON
