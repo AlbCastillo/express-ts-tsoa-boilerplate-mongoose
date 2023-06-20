2792cfc96bedf7f0889fbc99f76a787245dd9767
AlbCastillo <alberto.castillo@beoneoff.com>
2023-06-20 19:34:06 +0200

    refactor: update api components

--- src/middlewares/apiErrors.ts
+++ src/middlewares/apiErrors.ts
@@ -1,55 +1,56 @@
 import { Request, Response, NextFunction } from 'express';
 import { ValidateError } from 'tsoa';
-import { serializeError } from 'serialize-error';
+import { HttpError, HTTP_ERRORS } from '../utils/httpErrors';
 
 /**
  * API ERROR CLASS
  */
 export class ApiError extends Error {
-	statusCode: number;
+  readonly name: string;
+  readonly statusCode: number;
+  readonly message: string;
 
-	constructor({
-		name,
-		statusCode,
-		message,
-	}: {
-		name: string;
-		statusCode: number;
-		message?: string;
-	}) {
-		super(message);
-		this.name = name;
-		this.statusCode = statusCode;
-	}
+  constructor(httpError: HttpError, message?: string) {
+    const { name, statusCode }: HttpError = httpError;
+    const defaultMessage = httpError.message;
+    super(message || defaultMessage);
+    this.name = name;
+    this.statusCode = statusCode;
+    this.message = message || defaultMessage;
+  }
 }
 
-// eslint-disable-next-line consistent-return
 export function errorAPIHandler(
-	err: unknown,
-	req: Request,
-	res: Response,
-	next: NextFunction,
+  err: unknown | ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ): Response | void {
-	if (err instanceof ApiError) {
-		return res.status(err.statusCode).json({
-			name: err.name,
-			statusCode: err.statusCode,
-			message: serializeError(err),
-		});
-	}
-	if (err instanceof ValidateError) {
-		return res.status(422).json({
-			name: 'ValidationError',
-			statusCode: 422,
-			message: serializeError(err),
-		});
-	}
-	if (err instanceof Error) {
-		return res.status(500).json({
-			name: 'InternalServerError',
-			statusCode: 500,
-			message: serializeError(err),
-		});
-	}
-	next();
+  if (err instanceof ApiError) {
+    return res
+      .status(err.statusCode)
+      .json({ name: err.name, statusCode: err.statusCode, message: err.message });
+  }
+
+  if (err instanceof ValidateError) {
+    const { name, statusCode, message } = HTTP_ERRORS.VALIDATION_ERROR;
+    return res
+      .status(statusCode)
+      .json({ name, statusCode, message, details: err?.fields });
+  }
+
+  if (err instanceof Error) {
+    if (err.name === 'CastError') {
+      const { name, statusCode, message } = HTTP_ERRORS.BAD_REQUEST;
+      return res.status(statusCode).json({ name, statusCode, message });
+    }
+    if (err.name === 'NotFoundError') {
+      const { name, statusCode, message } = HTTP_ERRORS.NOT_FOUND;
+      return res.status(statusCode).json({ name, statusCode, message });
+    }
+    const { name, statusCode, message } = HTTP_ERRORS.INTERNAL_SERVER_ERROR;
+    return res.status(statusCode).json({ name, statusCode, message });
+  }
+
+  next();
 }
--- src/middlewares/morganLogger.ts
+++ src/middlewares/morganLogger.ts
@@ -1,9 +1,10 @@
 import morgan, { StreamOptions } from 'morgan';
+
 import logger from '../logging/winstonLogger';
 // Override the stream method by telling
 // Morgan to use our custom logger instead of the console.log.
 const stream: StreamOptions = {
-	write: (message) => logger.http(message),
+  write: message => logger.http(message),
 };
 
 // Skip all the Morgan http log if the
@@ -12,20 +13,20 @@ const stream: StreamOptions = {
 // we already told to the logger that it should print
 // only warning and error messages in production.
 const skip = () => {
-	const env = process.env.NODE_ENV || 'development';
-	return env !== 'development';
+  const env = process.env.NODE_ENV || 'development';
+  return env !== 'development';
 };
 
 // Build the morgan middleware
 const morganMiddlewareLogger = morgan(
-	// Define message format string (this is the default one).
-	// The message format is made from tokens, and each token is
-	// defined inside the Morgan library.
-	// You can create your custom token to show what do you want from a request.
-	':method :url :status :res[content-length] - :response-time ms',
-	// Options: in this case, I overwrote the stream and the skip logic.
-	// See the methods above.
-	{ stream, skip },
+  // Define message format string (this is the default one).
+  // The message format is made from tokens, and each token is
+  // defined inside the Morgan library.
+  // You can create your custom token to show what do you want from a request.
+  ':method :url :status :res[content-length] - :response-time ms',
+  // Options: in this case, I overwrote the stream and the skip logic.
+  // See the methods above.
+  { stream, skip },
 );
 
 export default morganMiddlewareLogger;
