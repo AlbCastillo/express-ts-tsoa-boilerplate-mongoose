e170e854e085a80e8729203be29f77caaaf42586
AlbCastillo <alberto.castillo@beoneoff.com>
2023-06-20 22:14:58 +0200

    refactor: refactor user

---
+++ src/api/v1/users/users.controller.ts
@@ -0,0 +1,42 @@
+import { Route, Controller, Get, Path, Post, SuccessResponse, Body } from 'tsoa';
+import { inject, injectable } from 'tsyringe';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserI } from './models/users.schema';
+import { UsersService } from './users.service';
+
+@injectable()
+@Route('v1/user')
+export class UserController extends Controller {
+  constructor(@inject(UsersService) private usersService: UsersService) {
+    super();
+  }
+
+  @Get('/get/{userId}')
+  @SuccessResponse('200', 'OK')
+  async getUser(@Path() userId: string): Promise<UserI> {
+    this.setStatus(200);
+    return this.usersService.findOne(userId);
+  }
+
+  @Get('/users')
+  @SuccessResponse('200', 'OK')
+  async getUsers(): Promise<UserI[]> {
+    this.setStatus(200);
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  @SuccessResponse('201', 'Created')
+  async createUser(@Body() user: CreateUserDto): Promise<UserI> {
+    this.setStatus(201);
+    return this.usersService.create(user);
+  }
+
+  @Post('/delete/{userId}')
+  @SuccessResponse('200', 'OK')
+  async deleteUser(@Path() userId: string): Promise<UserI> {
+    this.setStatus(200);
+    return this.usersService.remove(userId);
+  }
+}
